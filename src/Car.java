d0c5d3811be17f684e22e57420588019a5bada44
Moebius05 <Paul_Friedrich@gmx.at>
2018-11-16 07:40:02 +0100

    oobcsddscs

---
+++ src/Car.java
@@ -0,0 +1,12 @@
+//A Car has a brand. It also has the getBrand method.
+
+public class Car {
+    private String brand;
+
+    public Car(String brand){
+        this.brand = brand;
+    }
+    public String getBrand(){
+        return brand;
+    }
+}
